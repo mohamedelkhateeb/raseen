560419087175bdf40136e3abf9a0c567472cd43b
MohaMed Elkhateeb <100242324+mohamedelkhateeb@users.noreply.github.com>
2024-09-22 15:40:08 +0300

    search icon

--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -5,7 +5,7 @@ import { BiSearch } from "react-icons/bi";
 const SearchBox: React.FC = () => {
   return (
     <div className="relative">
-      <BiSearch color="#6D6D6D" size={20} className="absolute top-1/2 -translate-y-1/2 right-4" />
+      <BiSearch color="#6D6D6D" size={20} className="absolute mt-1 top-1/2 -translate-y-1/2 right-4" />
       <input
         type="text"
         placeholder="ابحث هنا عن أي فئة"
