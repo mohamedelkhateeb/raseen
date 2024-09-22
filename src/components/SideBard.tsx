edebb83b4d14cce1cd478a47db56dd96317d3135
MohaMed Elkhateeb <100242324+mohamedelkhateeb@users.noreply.github.com>
2024-09-22 15:32:45 +0300

    build success

--- src/components/SideBard.tsx
+++ src/components/SideBard.tsx
@@ -4,12 +4,8 @@ import Drawer from "@mui/material/Drawer";
 import Button from "@mui/material/Button";
 import List from "@mui/material/List";
 import Divider from "@mui/material/Divider";
-import ListItem from "@mui/material/ListItem";
-import ListItemButton from "@mui/material/ListItemButton";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import ListItemText from "@mui/material/ListItemText";
 import { TbListDetails } from "react-icons/tb";
-import { IoIosArrowBack, IoIosArrowDown } from "react-icons/io";
+import { IoIosArrowBack } from "react-icons/io";
 
 type Anchor = "top" | "left" | "bottom" | "right";
 
