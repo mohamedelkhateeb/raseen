9af18801c0e0abd4172ed81ab1ac3d19b3d3ac37
MohaMed Elkhateeb <100242324+mohamedelkhateeb@users.noreply.github.com>
2024-10-31 23:00:45 +0200

    bugfix: filter bug
    ref #010

--- src/services/companyService.ts
+++ src/services/companyService.ts
@@ -5,16 +5,16 @@ import Fetcher from './api/Fetcher';
 import { revalidateTag } from 'next/cache';
 
 export const getCompanies = async (filter: any, search?: string | null) => {
-  const selectedIds: string[] = filter?.sub_categories ? filter?.sub_categories.split(',') : [];  
+  const selectedIds: string[] = filter?.sub_categories ? filter?.sub_categories.split(',') : [];
   const formData = new FormData();
   if (search) formData.append('search', search);
   formData.append('category_id', filter?.category_id || 1);
   formData.append('sub_categories', JSON.stringify(selectedIds.map((id) => ({ id }))));
   formData.append('city_id', filter?.city_id || 1);
-  formData.append('min_price', filter?.min_price || 0);
-  formData.append('min_avg_rates', filter?.max_price || 0);
-  formData.append('max_price', filter?.min_avg_rates || 0);
-  formData.append('max_avg_rates', filter?.max_avg_rates || 0);
+  // formData.append('min_price', filter?.min_price || 0);
+  // formData.append('max_price', filter?.min_avg_rates || 0);
+  // formData.append('min_avg_rates', filter?.max_price || 0);
+  // formData.append('max_avg_rates', filter?.max_avg_rates || 0);
   try {
     const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}api/${search ? 'search' : 'filter'}`, {
       method: 'POST',
